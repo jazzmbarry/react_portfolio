206e6b46dcba938da61642f40b0296dc49e3fa1f
jazzmbarry <jazzmbarry@gmail.com>
2021-12-09 17:47:36 -0700

    updated About section

---
+++ src/components/About/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <section>
+            <div className="container">
+                <div className="flex-container">
+                    <div className="center" id="about">
+                        <h1 className="page-header">Resourcful learner * Full-Stack Developer * Musician * Personal Trainer</h1>
+                    </div>
+                    <div className="name-container">
+                        <h1 className="name">Matthew Barry</h1>
+                        <h2 className="profession">Full-Stack{<br />} Web Developer</h2>
+                    </div>
+                    <div className="about-container">
+                        <div className="about-section">
+                            <h3>About Me:</h3>
+                            <p>
+                                My name is Matthew Barry. I will complete a coding bootcamp in January 2022(pending). I have a degree in Exercise Science with a minor in Music. I also have three different personal training certificates as well as my CPR/AED First Aid certificate.
+                            </p>
+                            <p>
+                                I've always wanted to code but didn't have the courage to start until 2020 when I started self-teaching Python. Since then I've learned languages more attunded to Web Development such as HTML, CSS, and JavaScript. My goal with coding is to get into Deep and Machine learning to connect it to the hobbies and talents that I love. It facsinates me what can be done with modern technology. 
+                            </p>
+                            <p>
+                                I love working with JavaScript and Python. I don't have a preference in development and can work or learn what needs to be done.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
